2e170a9264b418aa7cb8026e5b7f3c35d3ec97a3
Sree Sushanth B V <bvsreesushanth29802@gmail.com>
2025-10-15 10:17:08 +0530

    Update Register.jsx

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/register", form);
+      await axios.post("https://module-end-assignment-5.onrender.com/api/auth/register", form);
       alert("Registered successfully!");
       navigate("/login");
     } catch (err) {
